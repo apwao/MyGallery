c59239fd6a3d71a9d38ef4e2a7683edd346e9f22
Sowayi <sapwao@gmail.com>
2019-06-14 11:28:48 -0400

    Add function test_instance to test correct instantiation of the class

--- places/tests.py
+++ places/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Image,tags,Location,Category
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    """
+    Class ImageTestClass to test the functions of 
+    """
+    def setUp(self):
+        """
+        setUp method to create an instance of Location
+        class to be used during testing
+        """
+        self.santorini=Location(name='Santorini',description='The warm sunny beaches of Greece')
+    
+    def test_instance(self):
+        """
+        test_instance method to check for the correct creation of 
+        an instance of Location
+        """ 
+        self.assertTrue(isinstance(self.santorini,Location))  
+    
+    def test_save_method(self):
+        """
+        test_save_method to check whether an instance of loation
+        is successfully stored to the database
+        """
