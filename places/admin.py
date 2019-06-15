9110bd84ca0aadd1ff881b432f7a77bc0896f99a
Sowayi <sapwao@gmail.com>
2019-06-14 22:31:50 -0400

    Register the Image,tags,Location and Category models with the Django administrator

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Image,Location,Category,tags
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(tags)
+
+
