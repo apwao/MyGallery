0dd79dacf9a74101c16cc33fa33e03d0720ffc5d
Sowayi <sapwao@gmail.com>
2019-06-16 22:01:41 -0400

    Register ImageAdmin to filter the tags

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Image,Location,Category,tags
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    """
+    """
+    filter_horizontal=('tags',)
+
+
 admin.site.register(Image)
 admin.site.register(Location)
 admin.site.register(Category)
